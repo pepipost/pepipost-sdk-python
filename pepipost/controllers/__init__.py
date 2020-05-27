18418c09b3485e77f52ed968bccaec6214b6d0ed
Vikram <snipperbytes@gmail.com>
2020-05-27 17:57:26 +0530

    adding support for all endpoints

---
+++ pepipost/controllers/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    'base_controller',
+    'mail_send_controller',
+    'events_controller',
+    'stats_controller',
+    'subaccounts_controller',
+    'subaccounts_delete_controller',
+    'subaccounts_get_sub_accounts_controller',
+    'setrecurringcreditddetails_controller',
+    'subaccounts_setsubaccountcredit_controller',
+    'subaccounts_update_subaccount_controller',
+    'subaccounts_create_subaccount_controller',
+    'suppression_controller',
+    'domain_delete_controller',
+    'domain_controller',
+]
